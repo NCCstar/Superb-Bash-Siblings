275489f2d6a5c41a089c10515b0e223c6e64c7ce
NCCstar <rhyspmiller@gmail.com>
2016-03-04 10:44:43 -0500

    Full NUMPAD

--- SBSPanel.java
+++ SBSPanel.java
@@ -115,7 +115,6 @@ public class SBSPanel extends JPanel
                      atks[0]=4;
                   }
       }
-   
       if(keyEn)
       {
          if(key8)
@@ -147,7 +146,37 @@ public class SBSPanel extends JPanel
                      atks[1]=4;
                   }
       }
-   
+      if(keyZe)
+      {
+         if(key8)
+         {
+            plys[1].attack(0,false);
+            atks[1]=0;
+         }
+         else
+            if(key6)
+            {
+               plys[1].attack(1,false);
+               atks[1]=1;
+            }
+            else
+               if(key5)
+               {
+                  plys[1].attack(2,false);
+                  atks[1]=2;
+               }
+               else
+                  if(key4)
+                  {
+                     plys[1].attack(3,false);
+                     atks[1]=3;
+                  }
+                  else
+                  {
+                     plys[1].attack (4,false);
+                     atks[1]=4;
+                  }
+      }
    }
    public void keyReleased(KeyEvent e)
    {
@@ -156,125 +185,125 @@ public class SBSPanel extends JPanel
    public void paintComponent(Graphics g)
    {
       super.paintComponent(g);
-         if(time+200>System.currentTimeMillis())//fps regulation
+      if(time+200>System.currentTimeMillis())//fps regulation
+      {
+         time=System.currentTimeMillis();
+         g.setColor(Color.white);
+         g.fillRect(0,0,getWidth(),getHeight());//background
+         g.setColor(Color.black);
+         draw(g,level.walls);//floor
+         g.setColor(Color.black);
+         moveCheck();//check keys
+         for(int i=0;i<plys.length;i++)
          {
-            time=System.currentTimeMillis();
-            g.setColor(Color.white);
-            g.fillRect(0,0,getWidth(),getHeight());//background
-            g.setColor(Color.black);
-            draw(g,level.walls);//floor
-            g.setColor(Color.black);
-            moveCheck();//check keys
-            for(int i=0;i<plys.length;i++)
+            int hitstun=plys[i].getHitstun();
+            if(hitstun>0)
+            {//increment hitstun
+               plys[i].setHitstun(--hitstun);
+            }
+            switch(i)//color based on player number
             {
-               int hitstun=plys[i].getHitstun();
-               if(hitstun>0)
-               {//increment hitstun
-                  plys[i].setHitstun(--hitstun);
-               }
-               switch(i)//color based on player number
-               {
-                  case 0:g.setColor(Color.red);
-                     break;
-                  case 1:g.setColor(Color.blue);
-                     break;
-                  default:g.setColor(Color.black);
-                     break;
-               }
-               if(hitstun>0)
-               {
-                  Color a=g.getColor();//hitstun color
-                  g.setColor(new Color(a.getRed(),a.getBlue(),a.getGreen(),150));
-               }
-               Rect[] rects = plys[i].getHitbox().getBoxes();
-               double vel=plys[i].getVelX();
-               if(vel!=0)
-               {
-                  double drag=0.02;
-                  if(vel>0)
-                     if(vel>drag)//resistance(slow to stop)
-                        plys[i].setVelX(vel-drag);
-                     else
-                        plys[i].setVelX(0);
+               case 0:g.setColor(Color.red);
+                  break;
+               case 1:g.setColor(Color.blue);
+                  break;
+               default:g.setColor(Color.black);
+                  break;
+            }
+            if(hitstun>0)
+            {
+               Color a=g.getColor();//hitstun color
+               g.setColor(new Color(a.getRed(),a.getBlue(),a.getGreen(),150));
+            }
+            Rect[] rects = plys[i].getHitbox().getBoxes();
+            double vel=plys[i].getVelX();
+            if(vel!=0)
+            {
+               double drag=0.02;
+               if(vel>0)
+                  if(vel>drag)//resistance(slow to stop)
+                     plys[i].setVelX(vel-drag);
                   else
-                     if(vel<(-1*drag))
-                        plys[i].setVelX(vel+drag);
-                     else
-                        plys[i].setVelX(0);
-               }        
-               plys[i].checkVel();//move the player
-               while(collide(plys[i].getHitbox().getBoxes(),level.walls))
-               {
-                  plys[i].getHitbox().offset(0,-.01);
-               }
-               Hitbox floorBox = plys[i].getHitbox();
-               floorBox.offset(0,.01);
-               if(collide(floorBox.getBoxes(),level.walls))
-               {
-                  plys[i].setInAir(false);
-               }
+                     plys[i].setVelX(0);
                else
-               {
-                  plys[i].setInAir(true);
-               }
-               floorBox.offset(0,-.01);
-               if(plys[i].isInAir())//if not on floor
-               {
-                  plys[i].fall();//gravity
-               }
-               else
-               {
-                  plys[i].setVelY(0);//not gravity(won't fall through floor)
-               }
-               int buffer=50;//buffer space for offscreen blast lines
-               if(plys[i].centerX()<buffer*-1||plys[i].centerX()>getWidth()+buffer||plys[i].centerY()<buffer*-1||plys[i].centerY()>getHeight()+buffer)
-               {//check blast lines
-                  plys[i].getHitbox().offsetTo(getWidth()/2,500);//if off screen, reset
-                  plys[i].setVelY(0);
-                  plys[i].setVelX(0);
-                  plys[i].addDamage(plys[i].getDamage()*-1);
-                  deaths[i]++;
-                  plys[i].setHitstun(500);//respawn immunity
+                  if(vel<(-1*drag))
+                     plys[i].setVelX(vel+drag);
+                  else
+                     plys[i].setVelX(0);
+            }        
+            plys[i].checkVel();//move the player
+            while(collide(plys[i].getHitbox().getBoxes(),level.walls))
+            {
+               plys[i].getHitbox().offset(0,-.01);
+            }
+            Hitbox floorBox = plys[i].getHitbox();
+            floorBox.offset(0,.01);
+            if(collide(floorBox.getBoxes(),level.walls))
+            {
+               plys[i].setInAir(false);
+            }
+            else
+            {
+               plys[i].setInAir(true);
+            }
+            floorBox.offset(0,-.01);
+            if(plys[i].isInAir())//if not on floor
+            {
+               plys[i].fall();//gravity
+            }
+            else
+            {
+               plys[i].setVelY(0);//not gravity(won't fall through floor)
+            }
+            int buffer=50;//buffer space for offscreen blast lines
+            if(plys[i].centerX()<buffer*-1||plys[i].centerX()>getWidth()+buffer||plys[i].centerY()<buffer*-1||plys[i].centerY()>getHeight()+buffer)
+            {//check blast lines
+               plys[i].getHitbox().offsetTo(getWidth()/2,500);//if off screen, reset
+               plys[i].setVelY(0);
+               plys[i].setVelX(0);
+               plys[i].addDamage(plys[i].getDamage()*-1);
+               deaths[i]++;
+               plys[i].setHitstun(500);//respawn immunity
                //reduce life count
-               }
-               draw(g,rects);
-               if(plys[i].getAtkUp())
+            }
+            draw(g,rects);
+            if(plys[i].getAtkUp())
+            {
+               Color a=g.getColor();
+               g.setColor(new Color(a.getRed(),a.getBlue(),a.getGreen(),100));
+               draw(g,plys[i].getAtk().getBoxes());
+               for(int q=0;q<plys.length;q++)
                {
-                  Color a=g.getColor();
-                  g.setColor(new Color(a.getRed(),a.getBlue(),a.getGreen(),100));
-                  draw(g,plys[i].getAtk().getBoxes());
-                  for(int q=0;q<plys.length;q++)
+                  if(i!=q)
                   {
-                     if(i!=q)
+                     if(collide(plys[i].getAtk().getBoxes(),plys[q].getHitbox().getBoxes()))
                      {
-                        if(collide(plys[i].getAtk().getBoxes(),plys[q].getHitbox().getBoxes()))
+                        if(plys[q].getHitstun()<=0)
                         {
-                           if(plys[q].getHitstun()<=0)
+                           if(i==0)
                            {
-                              if(i==0)
-                              {
-                                 plys[q].setVelY(plys[q].getAtkPow(atks[i],keySp,true)[0]*plys[q].getDamage()/30);
-                                 plys[q].setVelX(plys[q].getAtkPow(atks[i],keySp,true)[1]*plys[q].getDamage()/30);
-                                 plys[q].addDamage(plys[q].getAtkPow(atks[i],keySp,false)[0]);
-                              }
-                              else
-                              {
-                                 plys[q].setVelY(plys[q].getAtkPow(atks[i],keyEn,true)[0]*plys[q].getDamage()/30);
-                                 plys[q].setVelX(plys[q].getAtkPow(atks[i],keyEn,true)[1]*plys[q].getDamage()/30);
-                                 plys[q].addDamage(plys[q].getAtkPow(atks[i],keyEn,false)[0]);
-                              }
-                              plys[q].setHitstun(50);
+                              plys[q].setVelY(plys[q].getAtkPow(atks[i],keySp,true)[0]*plys[q].getDamage()/30);
+                              plys[q].setVelX(plys[q].getAtkPow(atks[i],keySp,true)[1]*plys[q].getDamage()/30);
+                              plys[q].addDamage(plys[q].getAtkPow(atks[i],keySp,false)[0]);
                            }
+                           else
+                           {
+                              plys[q].setVelY(plys[q].getAtkPow(atks[i],keyEn,true)[0]*plys[q].getDamage()/30);
+                              plys[q].setVelX(plys[q].getAtkPow(atks[i],keyEn,true)[1]*plys[q].getDamage()/30);
+                              plys[q].addDamage(plys[q].getAtkPow(atks[i],keyEn,false)[0]);
+                           }
+                           plys[q].setHitstun(50);
                         }
                      }
                   }
                }
-               g.setColor(Color.black);
-               g.setFont(new Font("Arial",0,20));
-               g.drawString(deaths[0]+":"+((int)plys[0].getDamage())+"%",getWidth()/3,getHeight());
-               g.drawString(deaths[1]+":"+((int)plys[1].getDamage())+"%",getWidth()*2/3,getHeight());
             }
+            g.setColor(Color.black);
+            g.setFont(new Font("Arial",0,20));
+            g.drawString(deaths[0]+":"+((int)plys[0].getDamage())+"%",getWidth()/3,getHeight());
+            g.drawString(deaths[1]+":"+((int)plys[1].getDamage())+"%",getWidth()*2/3,getHeight());
          }
+      }
       time=System.currentTimeMillis();
       repaint();
    }
@@ -347,18 +376,19 @@ public class SBSPanel extends JPanel
          case KeyEvent.VK_SHIFT:keySh=false;
             plys[0].attack(5,false);
             break;
-         case KeyEvent.VK_LEFT:key4=false;
+         case KeyEvent.VK_NUMPAD4:key4=false;
             break;
-         case KeyEvent.VK_DOWN:key5=false;
+         case KeyEvent.VK_NUMPAD5:key5=false;
             break;
-         case KeyEvent.VK_RIGHT:key6=false;
+         case KeyEvent.VK_NUMPAD6:key6=false;
             break;
-         case KeyEvent.VK_UP:key8=false;
+         case KeyEvent.VK_NUMPAD8:key8=false;
             break;
-         case KeyEvent.VK_CONTROL:keyEn=false;
+         case KeyEvent.VK_ENTER:keyEn=false;
             plys[1].attack(5,true);
             break;
-         case KeyEvent.VK_ENTER:keyZe=false;
+         case KeyEvent.VK_NUMPAD0:keyZe=false;
+            plys[1].attack(5,false);
             break;
          default: 
             break;
